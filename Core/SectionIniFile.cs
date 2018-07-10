3acaa6b2b8f6f6a289867111673afaa5b0281fb2
SHIBUYA K <shibuya@microbrains.co.jp>
2018-07-10 22:15:28 +0900

    IniFile, SectionIniFileにUpgradeメソッドを実装。

--- Core/SectionIniFile.cs
+++ Core/SectionIniFile.cs
@@ -74,91 +74,28 @@ public class SectionIniFile {
     /// </remarks>
     public bool Write(bool makebak=true, string tmpdir=null) {
         lock(m_mutex){
-            if(m_filename == null)
-                return false;
-            string tmpfilename;
-            if(String.IsNullOrEmpty(tmpdir)) {
-                tmpfilename = m_filename;
-            } else {
-                tmpfilename = Path.Combine(tmpdir,Path.GetFileName(m_filename));
-            }
-            tmpfilename += "."+rnd.Next(1000000).ToString();
-            try {
-                // まず元ファイルを一時ファイルにコピーする
-                if(File.Exists(m_filename)) {
-                    if(!FileUtil.Copy(m_filename, tmpfilename))
-                        return false;
-                    if(makebak)
-                        FileUtil.Copy(m_filename, m_filename+".bak");
-                }
-                using(StreamWriter sw = FileUtil.Writer(m_filename, m_enc)){
-                    if(sw == null)
-                        return false;
-                    if(File.Exists(tmpfilename)) {
-                        using(StreamReader sr = FileUtil.Reader(tmpfilename, m_enc)) {
-                            if(sr == null)
-                                return false;
-                            string sectionname = "";
-                            IniFile section = m_section[sectionname];
-                            while(!sr.EndOfStream) {
-                                string line = sr.ReadLine().Trim();
-                                if(line.StartsWith("#") || line.StartsWith(";")) {
-                                    sw.WriteLine(line);
-                                    continue;
-                                }
-                                if(line.StartsWith("[") && line.EndsWith("]")) {
-                                    // セクション内の残りのkey-valueを書き出す
-                                    foreach(string key in section.GetKeys()) {
-                                        sw.WriteLine(key+"="+section.Get(key));
-                                    }
-                                    m_section.Remove(sectionname);
-                                    sectionname = line.Substring(1,line.Length-2).Trim();
-                                    if(m_section.ContainsKey(sectionname))
-                                        section = m_section[sectionname];
-                                    else
-                                        section = new IniFile(); //ダミー
-                                    sw.WriteLine(line);
-                                    continue;
-                                }
-                                string[] keyval = line.Split("=".ToCharArray(), 2);
-                                if(keyval.Length == 2) {
-                                    string key = keyval[0].Trim();
-                                    sw.WriteLine(key+"="+section.Get(key, keyval[1].Trim()));
-                                    section.Remove(key);
-                                } else {
-                                    sw.WriteLine(line);
-                                }
-                            }
-                            sr.Close();
+            return _write(m_filename, makebak, tmpdir);
+        }
+    }
 
-                            // セクション内の残りのkey-valueを書き出す
-                            foreach(string key in section.GetKeys()) {
-                                sw.WriteLine(key+"="+section.Get(key));
-                            }
-                            m_section.Remove(sectionname);
-                        }
-                    }
-                    // 残りのセクションを書き出す
-                    foreach(string sectionname in m_section.Keys){
-                        if(sectionname != ""){
-                            sw.Write("[");
-                            sw.Write(sectionname);
-                            sw.WriteLine("]");
-                        }
-                        IniFile ini = m_section[sectionname];
-                        foreach(string key in ini.GetKeys()){
-                            sw.Write(key);
-                            sw.Write("=");
-                            sw.WriteLine(ini.Get(key));
-                        }
-                    }
-                    sw.Close();
-                }
-            } finally {
-                if(File.Exists(tmpfilename))
-                    File.Delete(tmpfilename);
-            }
-            return _reload();
+    /// <summary>
+    ///   INIファイルをアップグレードする。
+    /// </summary>
+    /// <param name="srcfile">アップグレードのベースとなるファイル。</param>
+    /// <param name="makebak">バックアップファイルを作るかどうか。</param>
+    /// <param name="tmpdir">一時保存ファイルを作るディレクトリ名。nullを指定したり省略した場合はINIファイルと同じディレクトリが使われる。</param>
+    /// <remarks>
+    ///   <para>
+    ///     ベースとなるファイルに書かれている内容のうち、本データベースで値を
+    ///     持っている定義行以外のすべての行は、そのまま保持される。（コメント行
+    ///     や空行は変更されない。）
+    ///     ベースファイルの定義値と本データベースの現在の値が異なる場合には、
+    ///     ベースファイルの定義行がコメントとして残る。
+    ///   </para>
+    /// </remarks>
+    public bool Upgrade(string srcfile, bool makebak=true, string tmpdir=null) {
+        lock(m_mutex) {
+            return _write(srcfile, makebak, tmpdir);
         }
     }
 
@@ -302,7 +239,184 @@ public class SectionIniFile {
         return true;
     }
 
+    private bool _write(string srcfile, bool makebak, string tmpdir) {
+        if(m_filename == null)
+            return false;
+        bool upgradeMode = (srcfile != m_filename);
+        string tmpfilename;
+        if(String.IsNullOrEmpty(tmpdir)) {
+            tmpfilename = m_filename;
+        } else {
+            tmpfilename = Path.Combine(tmpdir,Path.GetFileName(m_filename));
+        }
+        tmpfilename += "."+rnd.Next(1000000).ToString();
+        try {
+            // まず元ファイルを一時ファイルにコピーする
+            if(File.Exists(srcfile)) {
+                if(!FileUtil.Copy(srcfile, tmpfilename))
+                    return false;
+            }
+            if(makebak && File.Exists(m_filename))
+                FileUtil.Copy(m_filename, m_filename+".bak");
+            using(StreamWriter sw = FileUtil.Writer(m_filename, m_enc)){
+                if(sw == null)
+                    return false;
+                if(File.Exists(tmpfilename)) {
+                    using(StreamReader sr = FileUtil.Reader(tmpfilename, m_enc)) {
+                        if(sr == null)
+                            return false;
+                        string sectionname = "";
+                        IniFile section = m_section[sectionname];
+                        while(!sr.EndOfStream) {
+                            string line = sr.ReadLine().Trim();
+                            if(line.StartsWith("#") || line.StartsWith(";")) {
+                                sw.WriteLine(line);
+                                continue;
+                            }
+                            if(line.StartsWith("[") && line.EndsWith("]")) {
+                                // セクション内の残りのkey-valueを書き出す
+                                foreach(string key in section.GetKeys()) {
+                                    sw.WriteLine(key+"="+section.Get(key));
+                                }
+                                m_section.Remove(sectionname);
+                                sectionname = line.Substring(1,line.Length-2).Trim();
+                                if(m_section.ContainsKey(sectionname))
+                                    section = m_section[sectionname];
+                                else
+                                    section = new IniFile(); //ダミー
+                                sw.WriteLine(line);
+                                continue;
+                            }
+                            string[] keyval = line.Split("=".ToCharArray(), 2);
+                            if(keyval.Length == 2) {
+                                string key = keyval[0].Trim();
+                                string xval = keyval[1].Trim();
+                                string val = section.Get(key, xval);
+                                if(val == xval) {
+                                    sw.WriteLine(line);
+                                } else {
+                                    if(upgradeMode)
+                                        sw.WriteLine("#"+line);
+                                    sw.WriteLine(key+"="+val);
+                                }
+                                section.Remove(key);
+                            } else {
+                                sw.WriteLine(line);
+                            }
+                        }
+                        sr.Close();
+
+                        // セクション内の残りのkey-valueを書き出す
+                        foreach(string key in section.GetKeys()) {
+                            sw.WriteLine(key+"="+section.Get(key));
+                        }
+                        m_section.Remove(sectionname);
+                    }
+                }
+                // 残りのセクションを書き出す
+                foreach(string sectionname in m_section.Keys){
+                    if(sectionname != ""){
+                        sw.Write("[");
+                        sw.Write(sectionname);
+                        sw.WriteLine("]");
+                    }
+                    IniFile ini = m_section[sectionname];
+                    foreach(string key in ini.GetKeys()){
+                        sw.Write(key);
+                        sw.Write("=");
+                        sw.WriteLine(ini.Get(key));
+                    }
+                }
+                sw.Close();
+            }
+        } finally {
+            if(File.Exists(tmpfilename))
+                File.Delete(tmpfilename);
+        }
+        return _reload();
+    }
+
     private static Random rnd = new Random();
+
+#region SELFTEST
+#if SECTIONSELFTEST
+    public static int Main(string[] args) {
+        string inifilename = "SectionIniFileTest.ini";
+        
+        // サンプルのINIファイルを作る
+        using(StreamWriter sw = FileUtil.Writer(inifilename)) {
+            sw.WriteLine("# Sample INI file");
+            sw.WriteLine("[SectionA]");
+            sw.WriteLine("");
+            sw.WriteLine("hogehoge = mogera");
+            sw.WriteLine("hogehoge2 = mogera2");
+            sw.WriteLine("# hogehoge3 = mogera3");
+            sw.WriteLine("hogehoge4 = 314");
+            sw.WriteLine("");
+            sw.WriteLine("[SectionB]");
+            sw.WriteLine("");
+            sw.WriteLine("hogehogeB = mogeraB");
+            sw.WriteLine("hogehoge2B = mogera2B");
+            sw.WriteLine("# hogehoge3B = mogera3B");
+            sw.WriteLine("hogehoge4B = 1592");
+        }
+
+        // IniFileを作って、ちゃんと定義されているか確認
+        SectionIniFile ini = new SectionIniFile(inifilename);
+        if(ini.Get("SectionA","hogehoge") == "mogera")
+            Console.WriteLine("hogehoge: OK");
+        else
+            Console.WriteLine("hogehoge: NG");
+        if(ini.Get("SectionB","hogehoge2B") == "mogera2B")
+            Console.WriteLine("hogehoge2B: OK");
+        else
+            Console.WriteLine("hogehoge2B: NG");
+        if(ini.Get("SectionC","hogehoge") == "")
+            Console.WriteLine("hogehoge(SectionC): OK");
+        else
+            Console.WriteLine("hogehoge(SectionC): NG");
+        if(ini.Get("SectionA","hogehoge4",0) == 314)
+            Console.WriteLine("hogehoge4: OK");
+        else
+            Console.WriteLine("hogehoge4: NG");
+
+        // 定義を追加して書き出してみる
+        ini.Set("SectionA", "piyopiyo", "punyo");
+        ini.Set("SectionA", "hogehoge4", 512);
+        ini.Set("SectionC", "hogehoge", "mogeraC");
+        ini.Write();
+        using(StreamReader sr = FileUtil.Reader(inifilename)) {
+            while(!sr.EndOfStream) {
+                Console.WriteLine(sr.ReadLine());
+            }
+        }
+
+        // Upgradeの試験
+        using(StreamWriter sw = FileUtil.Writer(inifilename+".dist")) {
+            sw.WriteLine("# Upgraded Sample INI file");
+            sw.WriteLine("");
+            sw.WriteLine("[SectionA]");
+            sw.WriteLine("hogehoge = mogerachu");
+            sw.WriteLine("hogehoge2 = mogera2");
+            sw.WriteLine("# added hogehoge3.");
+            sw.WriteLine("hogehoge3 = mogera3");
+            sw.WriteLine("hogehoge4 = 314");
+            sw.WriteLine("");
+            sw.WriteLine("[SectionC]");
+            sw.WriteLine("hogehoge = alpha");
+            sw.WriteLine("hogehogeC = beta");
+        }
+        ini.Upgrade(inifilename+".dist");
+        using(StreamReader sr = FileUtil.Reader(inifilename)) {
+            while(!sr.EndOfStream) {
+                Console.WriteLine(sr.ReadLine());
+            }
+        }
+
+        return 0;
+    }
+#endif
+#endregion
 
 }
 
