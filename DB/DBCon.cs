83461b16f26c3c6c373ac480dc4920ae46f08b1e
SHIBUYA K <shibuya@ncad.co.jp>
2019-11-26 17:24:27 +0900

    年齢を算出するSQLカラム定義文字列を作る

--- DB/DBCon.cs
+++ DB/DBCon.cs
@@ -811,6 +811,67 @@ public class DBCon : Loggable, IDisposable {
     }
 
     /// <summary>
+    ///   誕生日から年齢を算出するカラム定義文字列を得る
+    /// </summary>
+    /// <param name="columnName">年齢カラムに付ける名前</param>
+    /// <param name="birthdayColumnName">誕生日のカラム名</param>
+    /// <param name="baseDate">基準日</param>
+    /// <remarks>
+    ///   <para>
+    ///     誕生日が1000年より前だったり、基準日より後だったりするときは、0を返します。
+    ///   </para>
+    /// </remarks>
+    public static string AgeColumn(string columnName, string birthdayColumnName, DateTime baseDate) {
+        return String.Format("CASE WHEN `{0}` IS NULL THEN 0 WHEN YEAR(`{0}`)<1000 THEN 0 WHEN `{0}`>'{1:D4}-{2:D2}-{3:D2}' THEN 0 ELSE TRUNCATE(({4}-YEAR(`{0}`)*10000-MONTH(`{0}`)*100-DAYOFMONTH(`{0}`))/10000,0) END AS {5}",
+                             birthdayColumnName, baseDate.Year, baseDate.Month, baseDate.Day, baseDate.Year*10000+baseDate.Month*100+baseDate.Day, columnName);
+    }
+
+    /// <summary>
+    ///   誕生日から年齢を算出するカラム定義文字列を得る
+    /// </summary>
+    /// <param name="columnName">年齢カラムに付ける名前</param>
+    /// <param name="birthdayColumnName">誕生日のカラム名</param>
+    /// <remarks>
+    ///   <para>
+    ///     誕生日が1000年より前だったり、基準日より後だったりするときは、0を返します。
+    ///   </para>
+    /// </remarks>
+    public static string AgeColumn(string columnName, string birthdayColumnName) {
+        return AgeColumn(columnName, birthdayColumnName, DateTime.Today);
+    }
+
+    /// <summary>
+    ///   誕生日から学年年齢を算出するカラム定義文字列を得る
+    /// </summary>
+    /// <param name="columnName">年齢カラムに付ける名前</param>
+    /// <param name="birthdayColumnName">誕生日のカラム名</param>
+    /// <param name="baseDate">基準日</param>
+    /// <remarks>
+    ///   <para>
+    ///     学年年齢とは、基準日が1月～3月の時はその前年の4月1日時点の年齢を、
+    ///     4月～12月の時はその年の4月1日時点の年齢を指します。
+    ///   </para>
+    /// </remarks>
+    public static string SchoolAgeColumn(string columnName, string birthdayColumnName, DateTime baseDate) {
+        return AgeColumn(columnName, birthdayColumnName, new DateTime((baseDate.Month<4)?(baseDate.Year-1):baseDate.Year, 4, 1));
+    }
+    
+    /// <summary>
+    ///   誕生日から学年年齢を算出するカラム定義文字列を得る
+    /// </summary>
+    /// <param name="columnName">年齢カラムに付ける名前</param>
+    /// <param name="birthdayColumnName">誕生日のカラム名</param>
+    /// <remarks>
+    ///   <para>
+    ///     学年年齢とは、本日が1月～3月の時は前年の4月1日時点の年齢を、
+    ///     4月～12月の時は本年の4月1日時点の年齢を指します。
+    ///   </para>
+    /// </remarks>
+    public static string SchoolAgeColumn(string columnName, string birthdayColumnName) {
+        return SchoolAgeColumn(columnName, birthdayColumnName, DateTime.Today);
+    }
+
+    /// <summary>
     ///   SQLを実行して実行結果をテキストストリームにHumanReadableな形で書き出す
     /// </summary>
     public void Execute(string sql, TextWriter w) {
