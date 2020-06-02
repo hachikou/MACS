0dfa9bc338868e23cfc0122c751cf27657035b8d
SHIBUYA K <shibuya@ncad.co.jp>
2020-06-03 08:28:49 +0900

    DropDown互換のRadioSelectorを実装。

--- HttpServer/RadioSelector.cs
+++ HttpServer/RadioSelector.cs
@@ -82,10 +82,10 @@ public class RadioSelector<T> : TranslatableWebControl
     }
 
     /// <summary>
-    /// 　ラジオボタン Enum名表示
-    /// 　<remarks>
-    /// 　　true:Enum名表示 false:Enum名非表示
-    /// 　</remarks>
+    ///   ラジオボタン Enum名表示
+    ///   <remarks>
+    ///     true:Enum名表示 false:Enum名非表示
+    ///   </remarks>
     /// </summary>
     public bool ShowEnumName = true;
 
@@ -181,6 +181,245 @@ public class RadioSelector<T> : TranslatableWebControl
         }
     }
 
-}
+} // End of class RadioSelector<T>
+
+public class RadioSelector : TranslatableWebControl {
+
+    /// <summary>
+    ///   値の配列
+    /// </summary>
+    public string[] Values;
+
+    /// <summary>
+    ///   表示文字列の配列
+    /// </summary>
+    public string[] Labels;
+
+    /// <summary>
+    ///   値がすでに存在するかどうか
+    /// </summary>
+    public bool ContainsValue(string txt) {
+        if(Values == null)
+            return false;
+        foreach(string x in Values) {
+            if(x == txt)
+                return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    ///   表示文字列がすでに存在するかどうか
+    /// </summary>
+    public bool ContainsLabel(string txt) {
+        if(Labels == null)
+            return false;
+        foreach(string x in Labels) {
+            if(x == txt)
+                return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    ///   表示文字列、値を追加する
+    /// </summary>
+    public void AddItem(string label, string value) {
+        if((Labels == null) || (Values == null)) {
+            Labels = new string[1]{label};
+            Values = new string[1]{value};
+            return;
+        }
+        string[] xlabels = new string[Labels.Length+1];
+        string[] xvalues = new string[Values.Length+1];
+        for(int i = 0; i < Labels.Length; i++)
+            xlabels[i] = Labels[i];
+        for(int i = 0; i < Values.Length; i++)
+            xvalues[i] = Values[i];
+        xlabels[xlabels.Length-1] = label;
+        xvalues[xvalues.Length-1] = value;
+        Labels = xlabels;
+        Values = xvalues;
+    }
+
+    /// <summary>
+    ///   全ての値と表示文字列のペアを "値:表示文字列,値:表示文字列,..."という文字列にまとめたもの
+    /// </summary>
+    public string List {
+        get {
+            StringBuilder sb = new StringBuilder();
+            if(Values == null)
+                return "";
+            if(Labels == null)
+                Labels = Values;
+            for(int i = 0; i < Values.Length; i++){
+                if(sb.Length > 0)
+                    sb.Append(',');
+                sb.Append(Values[i]);
+                sb.Append(':');
+                sb.Append(Labels[i]);
+            }
+            return sb.ToString();
+        }
+        set {
+            string[] list = value.Split(",".ToCharArray());
+            Values = new string[list.Length];
+            Labels = new string[list.Length];
+            for(int i = 0; i < list.Length; i++){
+                string[] lv = list[i].Split(":".ToCharArray(),2);
+                if(lv.Length == 2){
+                    Values[i] = lv[0];
+                    Labels[i] = lv[1];
+                }else{
+                    Values[i] = list[i];
+                    Labels[i] = list[i];
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    ///   選択されている項目の番号
+    /// </summary>
+    public int SelectedIndex {
+        get {
+            if(Values == null)
+                return -1;
+            string v = (Value == null)?"":Value.ToString();
+            for(int i = 0; i < Values.Length; i++){
+                if(Values[i] == v)
+                    return i;
+            }
+            return 0;
+        }
+        set {
+            if(Values == null)
+                return;
+            if(value < 0)
+                Value = Values[0];
+            else if(value < Values.Length)
+                Value = Values[value];
+            else
+                Value = Values[Values.Length-1];
+        }
+    }
+
+    /// <summary>
+    ///   選択された値
+    /// </summary>
+    public string Selected {
+        get { return (Value == null)?"":Value.ToString(); }
+        set { Value = value; }
+    }
+
+    /// <summary>
+    ///   選択されている項目の表示文字列
+    /// </summary>
+    public string Text {
+        get {
+            string v = (Value == null)?"":Value.ToString();
+            for(int i = 0; i < Values.Length; i++){
+                if(Values[i] == v)
+                    return Labels[i];
+            }
+            return v;
+        }
+    }
+    
+    /// <summary>
+    ///   縦に並べるかどうか
+    /// </summary>
+    public bool Vertical = false;
+
+
+    /// <summary>
+    ///   コンストラクタ
+    /// </summary>
+    public RadioSelector(string name, string id, object value) : base(name, id) {
+        Value = value;
+    }
+
+    /// <summary>
+    ///   コンストラクタ
+    /// </summary>
+    public RadioSelector(string name, string value) : base(name) {
+        Value = value;
+    }
+
+    /// <summary>
+    ///   コンストラクタ
+    /// </summary>
+    public RadioSelector(string name) : base(name) {}
+
+    /// <summary>
+    ///   デフォルトコンストラクタ
+    /// </summary>
+    public RadioSelector() : base() {}
+
+
+    /// <summary>
+    ///   レンダリング
+    /// </summary>
+    public override StringBuilder Render(StringBuilder sb) {
+        if(!Visible) {
+            sb.Append("<input type=\"hidden\" name=\"");
+            sb.Append(Name);
+            sb.Append("\" value=\"");
+            sb.Append(Selected.ToString());
+            sb.Append("\"");
+            if(!String.IsNullOrEmpty(ID)) {
+                sb.Append(" id=\"");
+                sb.Append(ID);
+                sb.Append("\"");
+            }
+            sb.Append("/>");
+            return sb;
+        }
+        sb.Append("<div class=\"radiogroup");
+        if(CssClass != null){
+            sb.Append(" ");
+            sb.Append(CssClass);
+        }
+        sb.Append("\"");
+        if(!String.IsNullOrEmpty(ID)) {
+            sb.Append(" id=\"");
+            sb.Append(ID);
+            sb.Append("\"");
+        }
+        sb.Append(">");
+        for(int i = 0; i < Labels.Length; i++) {
+            string label = Labels[i];
+            string val = (i < Values.Length)?Values[i]:Values[Values.Length-1];
+            string id = Name+"-"+val;
+            sb.Append("<span class=\"radio\" style=\"white-space:nowrap\">");
+            sb.Append("<input type=\"radio\" name=\"");
+            sb.Append(Name);
+            sb.Append("\" id=\"");
+            sb.Append(id);
+            sb.Append("\" value=\"");
+            sb.Append(val);
+            sb.Append("\"");
+            if(val == Selected.ToString())
+                sb.Append(" checked=\"checked\"");
+            if(!String.IsNullOrEmpty(OnClick)) {
+                sb.Append(" onclick=\"");
+                sb.Append(OnClick);
+                sb.Append("\"");
+            }
+            sb.Append("/><label for=\"");
+            sb.Append(id);
+            sb.Append("\">");
+            sb.Append(HE(label));
+            sb.Append("</label></span>");
+            if(Vertical)
+                sb.Append("<br/>");
+            else
+                sb.Append(" ");
+        }
+        sb.Append("</div>");
+        RenderInLineError(sb);
+        return sb;
+    }
+} // End of class RadioSelector
 
 } // End of namespace
